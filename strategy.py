42247e166ffb5e1c041d9f582f0458d1e744aae0
_markwong_ <markhmwong@users.noreply.github.com>
2024-03-01 11:27:56 +1100

    Create strategy.py

---
+++ strategy.py
@@ -0,0 +1,142 @@
+# Assuming python3 usage for interface implementation
+# This will let you scale your code if additional api implementations are necessary
+# just add the concrete class with it's own concrete algorithm. Determine what api method is used at "Main" and just instatiate and call execute
+
+# ABC - Abstract Base Classes
+from abc import ABC, abstractmethod 
+
+
+class ApiUtil():
+
+    def getApi():
+        pass
+
+#create abstract base class. All apis should have a binding contract to these methods
+class ApiDeploymentImplementation(ABC):
+
+    @abstractmethod
+    def run(self):
+        pass
+    
+    @abstractmethod
+    def readInputs():
+        pass
+    
+    @abstractmethod
+    def delete():
+        pass
+
+    @abstractmethod
+    def rollback():
+        pass
+
+    @abstractmethod
+    def backout():
+        pass
+
+    # add more common methods as necessary. some implementation
+
+"""
+Api deployment concrete classes
+"""
+
+class Db2(ApiDeploymentImplementation):
+    def __init__(self, apiUtil: ApiUtil) -> None:
+        print("This is db2")
+        self.apiUtil = apiUtil
+
+    def run(self):
+        print("running db2")
+        pass
+    
+    
+    def readInputs():
+        pass
+    
+    
+    def delete():
+        pass
+
+    
+    def rollback():
+        pass
+
+    
+    def backout():
+        pass
+
+class Inbound(ApiDeploymentImplementation):
+
+    def __init__(self, apiUtil: ApiUtil) -> None:
+        print("This is inbound")
+        self.apiUtil = apiUtil
+
+    def run(self):
+        print("running inbound")
+        pass
+
+    def readInputs():
+        pass
+    
+    
+    def delete():
+        pass
+
+    
+    def rollback():
+        pass
+
+    
+    def backout():
+        pass
+
+class Outbound(ApiDeploymentImplementation):
+
+    def __init__(self, apiUtil: ApiUtil) -> None:
+        print("This is outbound")
+        self.apiUtil = apiUtil
+
+    def run(self):
+        print("running outbound")
+        pass
+
+    def readInputs():
+        pass
+    
+    
+    def delete():
+        pass
+
+    
+    def rollback():
+        pass
+
+    
+    def backout():
+        pass
+
+
+
+class ApiStrategy():
+
+    def __init__(self, apiType: ApiDeploymentImplementation) -> None:
+        self.apiType = apiType
+    
+    def deploy(self):
+        self.apiType.run()
+
+"""
+Main stuff
+"""
+
+if __name__ == "__main__":
+    apiType = "outbound"
+    if apiType == "outbound":
+        print("outbound")
+
+        outboundStrat = ApiStrategy(Outbound(ApiUtil()))
+        outboundStrat.deploy()
+        inboundStrat = ApiStrategy(Inbound(ApiUtil()))
+        inboundStrat.deploy()
+        db2Strat = ApiStrategy(Db2(ApiUtil()))
+        db2Strat.deploy()
